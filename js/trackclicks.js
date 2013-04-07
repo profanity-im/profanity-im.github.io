fdc8fa8b269bb835b424787aef8b8437b2401aae
James Booth <boothj5@gmail.com>
2013-04-07 22:46:08 +0100

    Added brew formula link

--- js/trackclicks.js
+++ js/trackclicks.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
     $("#link-mailinglist").click(function (e) {
-       _gaq.push(['_trackEvent', 'links', 'clicked', "mailing list"]);
+        _gaq.push(['_trackEvent', 'links', 'clicked', "mailing list"]);
     });
 
     $("#link-github").click(function (e) {
-       _gaq.push(['_trackEvent', 'links', 'clicked', "github"]);
+        _gaq.push(['_trackEvent', 'links', 'clicked', "github"]);
+    });
+
+    $("#robroom-brew").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "mac-brew"]);
     });
 });
