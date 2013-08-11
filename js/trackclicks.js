d66590738648fbd1be91ac6280d44c680d1e1041
James Booth <boothj5@gmail.com>
2013-08-12 00:28:00 +0100

    Added googleplus and twitter links

--- js/trackclicks.js
+++ js/trackclicks.js
@@ -7,6 +7,14 @@ $(document).ready(function () {
         _gaq.push(['_trackEvent', 'links', 'clicked', "github"]);
     });
 
+    $("#link-googleplus").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "google plus"]);
+    });
+
+    $("#link-twitter").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "twitter"]);
+    });
+
     $("#robroom-brew").click(function (e) {
         _gaq.push(['_trackEvent', 'links', 'clicked', "mac-brew"]);
     });
