297f7f2fb30aab490a747e5a258513f21f07e3ad
James Booth <boothj5@gmail.com>
2013-11-19 00:09:50 +0000

    Added helpout page

--- js/trackclicks.js
+++ js/trackclicks.js
@@ -7,6 +7,22 @@ $(document).ready(function () {
         _gaq.push(['_trackEvent', 'links', 'clicked', "github"]);
     });
 
+    $("#link-githubissues").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "github issues"]);
+    });
+
+    $("#link-helpout").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "help out"]);
+    });
+
+    $("#link-websitesource").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "website source"]);
+    });
+
+    $("#link-otr").click(function (e) {
+        _gaq.push(['_trackEvent', 'links', 'clicked', "otr"]);
+    });
+
     $("#link-googleplus").click(function (e) {
         _gaq.push(['_trackEvent', 'links', 'clicked', "google plus"]);
     });
