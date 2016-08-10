2e33cbe4936ef687ea7aa2ab98da38a7ec6fe31e
James Booth <boothj5@gmail.com>
2016-08-10 22:02:05 +0100

    Update plugins API docs

--- plugins/c/html/search/typedefs_0.js
+++ plugins/c/html/search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cmd_5fcb',['CMD_CB',['../profapi_8h.html#af45aab517cb359fdd14f95eb824d29c7',1,'profapi.h']]]
+  ['cmd_5fcb',['CMD_CB',['../profapi_8h.html#a8bbaab48137debe4a06f0e7f69e3e717',1,'profapi.h']]]
 ];
